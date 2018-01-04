1790636a487462cc52ce8b4a5edab2a6a87b35a2
Edward Norton <edwardnorton@icloud.com>
2018-01-04 22:31:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(musicians, instruments) {
   return array
 }
 
-function johnLennonFacts(facts) {
+function johnLennonFacts(array) {
   const facts = [
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
